721bf391d2d1f0883b786a9824607d145741b6ca
David Alecrim <david.alecrim1@gmail.com>
2017-10-06 16:59:23 +0100

    try

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import ReactAudioAssistant from './src/components/react_audio_assistant';
 import Reducer from './src/reducers/react_audio_assistant_reducer';
 
-export {
-    ReactAudioAssistant,
-    Reducer,
-};
+exports.ReactAudioAssistant = ReactAudioAssistant;
+exports.Reducer = Reducer;
